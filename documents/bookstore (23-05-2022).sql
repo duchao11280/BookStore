4403605bebe2815168e1c807b2b078150ee82583
Le Duc Thang <67146524+ldthang2201@users.noreply.github.com>
2022-05-23 22:31:10 +0700

    Update DB

---
+++ documents/bookstore (23-05-2022).sql
@@ -0,0 +1,145 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : LDT
+ Source Server Type    : MariaDB
+ Source Server Version : 100417
+ Source Host           : localhost:3306
+ Source Schema         : bookstore
+
+ Target Server Type    : MariaDB
+ Target Server Version : 100417
+ File Encoding         : 65001
+
+ Date: 23/05/2022 22:30:35
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for books
+-- ----------------------------
+DROP TABLE IF EXISTS `books`;
+CREATE TABLE `books`  (
+  `bookId` int(11) NOT NULL AUTO_INCREMENT,
+  `bookName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `auth` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `tinyDescription` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `description` text CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `language` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `year` int(11) NULL DEFAULT NULL,
+  `nxb` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `price` int(11) NULL DEFAULT NULL,
+  `quantity` int(11) NULL DEFAULT NULL,
+  `subCatId` int(11) NULL DEFAULT NULL,
+  `sale` float NULL DEFAULT 1,
+  `coverImg` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `thumbnails` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `isDisable` tinyint(4) NULL DEFAULT 0,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`bookId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for categories
+-- ----------------------------
+DROP TABLE IF EXISTS `categories`;
+CREATE TABLE `categories`  (
+  `catId` int(11) NOT NULL AUTO_INCREMENT,
+  `catName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `thumbnails` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `isDisable` tinyint(4) NULL DEFAULT 0,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`catId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for favoritelist
+-- ----------------------------
+DROP TABLE IF EXISTS `favoritelist`;
+CREATE TABLE `favoritelist`  (
+  `bookId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`bookId`, `userId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for orderdetails
+-- ----------------------------
+DROP TABLE IF EXISTS `orderdetails`;
+CREATE TABLE `orderdetails`  (
+  `orderId` int(11) NOT NULL,
+  `bookId` int(11) NOT NULL,
+  `price` int(11) NULL DEFAULT NULL,
+  `number` int(11) NULL DEFAULT NULL,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`orderId`, `bookId`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for orders
+-- ----------------------------
+DROP TABLE IF EXISTS `orders`;
+CREATE TABLE `orders`  (
+  `orderId` int(11) NOT NULL AUTO_INCREMENT,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  `fullName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `status` int(11) NULL DEFAULT 0,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`orderId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for rating
+-- ----------------------------
+DROP TABLE IF EXISTS `rating`;
+CREATE TABLE `rating`  (
+  `rateId` int(11) NOT NULL AUTO_INCREMENT,
+  `bookId` int(11) NULL DEFAULT NULL,
+  `userName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `rate` float NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`rateId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 4 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for subcategories
+-- ----------------------------
+DROP TABLE IF EXISTS `subcategories`;
+CREATE TABLE `subcategories`  (
+  `subCatId` int(11) NOT NULL AUTO_INCREMENT,
+  `subCatName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `catId` int(11) NULL DEFAULT NULL,
+  `isDisable` tinyint(4) NULL DEFAULT 0,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`subCatId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Table structure for users
+-- ----------------------------
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users`  (
+  `userId` int(11) NOT NULL AUTO_INCREMENT,
+  `fullName` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `phone` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `password` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `role` tinyint(4) NULL DEFAULT 1,
+  `address` varchar(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci NULL DEFAULT NULL,
+  `createAt` datetime NULL DEFAULT current_timestamp(),
+  `updateAt` datetime NULL DEFAULT current_timestamp() ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`userId`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 14 CHARACTER SET = utf8 COLLATE = utf8_unicode_ci ROW_FORMAT = Dynamic;
+
+SET FOREIGN_KEY_CHECKS = 1;
